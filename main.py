c062b16ffa130c45ec04147ec02781b9c180f3ee
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-10-01 16:17:44 +0000

    # def function 2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-# function definition
-def say_hello(): 
-  print("Hello how are you?")
+# user_name 에 함수가 실행될 때마다의 parameter / argument 를 저장한다. 
+def say_hello(user_name):
+  print("hellow ~",user_name," haw ar you?")
 
-say_hello()
-say_hello()
-say_hello()
+# 스코프 내에서만 유용하다. 
+say_hello("user_name_test")
+say_hello("lynn")
+say_hello("lewis")
+
+def say_bye():
+  print("bye bye")
+
+
+say_bye()
+
+
