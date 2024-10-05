15154a40253b6e31f3267923c451b8f02101815c
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-10-05 14:22:39 +0000

    if elif else

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-# conditional statement
-a = 11
-# checking the value is same or not
-if a == 10:
-  print("true")
+winner = 666
+
+if winner <= 10:
+  print("if ")
+elif winner <= 25:
+  print("elif")
+elif winner == 0:
+  print("elif 2")
+elif winner == 50:
+  print("elif 3")
 else:
-  print("false")
-
-password_correct = False
-
-if password_correct:
-  print("here is your money")
-else:
-  print("sorry, wrong password")
-
-
-winner = 10
-
-if winner > 10:
-  print(f"{winner} is greater than 10")
-elif winner < 10:
-  print(f"{winner} is less than 10")
-else:
-  print(f'{winner} is 10')
+  print("else the other one")
