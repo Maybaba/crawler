d32e20bd8cdaa1964ad222bd5c15942c33191ce8
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-10-05 13:51:41 +0000

    return statement 2

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # tax calculator
-
+# return statement can use return value anywhere
 def tax_calc(money):
   return money * 0.35
 
 def pay_tax(tax):
-  print("thank you for paying", tax)
+  print("thx for paying", tax)
 
-tax_calc(13000)
-pay_tax(tax_calc(13000))
-
+pay_tax(tax_calc(120))
