5c45d227bf2691785f7e6cf1878d9ca1b0f1c944
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-10-05 14:04:43 +0000

    return statement 3

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-# insert param into string value
+# machine that making juice
+def make_juice(fruit):
+  return f"{fruit} + 🥤"
 
-name = "may"
-age = 26
-color = "brown"
+def add_ice(juice):
+  return f"{juice} + 🥶"
 
-print (f"hello i am {name} and i am {age} also i have {color} eyes")
+def add_sugar(iced_juice):
+  return f"{iced_juice} + ✰"
+
+juice = make_juice("🍓")
+cold_juice = add_ice(juice)
+perfecto_juice = add_sugar(cold_juice)
+
+print(perfecto_juice)
+
+# keword return kills the code after it
