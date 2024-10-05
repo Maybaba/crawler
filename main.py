b46f402f7a125e3e203aeda0821a1c82c5f70571
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-10-05 14:46:33 +0000

    true or false

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ elif age > 17 and age < 21:
   print("You are in the prime of your life")
 else:
   print("go vote")
+
+True and True == True
+False and True == False
+True and False == False
+False and False == False
+
+True or True == True
+True or False == True
+False or True == True
+False or False == False
