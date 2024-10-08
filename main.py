fa7dbad509c0fe35858457842e743aa99d546d25
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-10-08 03:47:28 +0000

    # randint

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-
-
-# how to ignore code : use hashtag like this
-
-# or use three quotation marks like this
-
-"""
-
 from random import randint
-
-user_choice = int(input("Choose number . . . "))
 
 pc_choice = randint(1, 50)
 
-if user_choice == pc_choice:
+playing = True
+
+while playing:
+ user_choice = int(input("Choose number . . .(1~50) "))
+ if user_choice == pc_choice:
   print("you won !")
-elif user_choice > pc_choice:
-  print("Higher! Computer chose", pc_choice)
-elif user_choice < pc_choice:
-  print("Lower! Computer chose", pc_choice)
+  playing = False
+ elif user_choice > pc_choice:
+  print("Lower")
+ elif user_choice < pc_choice:
+  print("Higher")
 
-"""
-
-print("hi")
-
-# <while> is same as <if> except that it will keep going until the condition is false
-
-if True:
-  print("Hi i am true : D ")
-
-distance = 0
-
-while distance < 20:
-  print("i am running : ", distance , "km")
-  distance = distance + 1
-
-#this code repeat until the distance is 20
 
