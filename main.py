6e1fef422e4745412b34be76bfad1d5f56bc5b42
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-10-09 15:04:05 +0000

    # recap data structure

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-# Dicts data structure
+# function : unbounded with data (modifiable)
 
-# key value pair
+# method : bounded with data (unmodifiable)
+
+print("May".encode("utf-8"))
+
+numbers = [5, 4, 3, 2, 1, "True", True, 12, ["☕️", "🎃", "🎃", "🎃", "🎃", "🎃"]]
+
+numbers.append("🪼")
+
+print(numbers)
+print(numbers[3])
+
+# tuple () dict {} list []
 
 player = {
-  'name': 'May',
-  'age': 25,
-  'alive': True,
-  'fav_food': ["🎃", "☕️"]
+  "name":"May",
+  "age" : 25,
+  "alive" : True,
+  "fav_food" : ["🥦", "🍓"],
+  "friend" : {
+    "name" : "Jay",
+    "fav_food" : ["🍕", "🍔"]
+  }
 }
 
-print(player.get('age'))
-print(player.get('fav_food'))
-print(player['fav_food'])
-print(player['fav_food'][0])
+print(player)
 
-player.pop('name')
+player['fav_food'] = "☕️"
+player.pop("alive")
+
+print(player['friend']['fav_food'].append("🧃"))
+
 print(player)
-player['xp'] = 1500
-print(player)
-player['fav_food'].append("❤︎")
-print(player.get('fav_food'))
-print(player['fav_food'])
+
