1932582011a6e1b952b0b0959e513d90680110f9
Maybaba <sinyunjong@gmail.com>
2024-11-01 06:10:20 +0900

    # calculator

--- main.py
+++ main.py
@@ -33,3 +33,37 @@ print(player['friend']['fav_food'].append("🧃"))
 
 print(player)
 
+def calculator():
+  print("Happy halloween 🎃")
+
+  while True:
+      first_number = input("Choose a number :")
+      second_number = input("Choose another one :")
+
+      print("Enter the operation +, -, * and / if you want to exit enter 'exit'")
+      operation = input()
+      if operation == "exit":
+        break
+
+        # transform the string to int
+        try:
+            first_number = int(first_number)
+            second_number = int(second_number)
+        except ValueError:
+          print("Invalid number")
+          continue
+
+      if operation == "+":
+        print(int(first_number) + int(second_number))
+      elif operation == "-":
+        print(int(first_number) - int(second_number))
+      elif operation == "*":
+        print(int(first_number) * int(second_number))
+      elif operation == "/":
+        print(int(first_number) / int(second_number))
+      else:
+        print("Invalid operation")
+
+calculator()
+
+
